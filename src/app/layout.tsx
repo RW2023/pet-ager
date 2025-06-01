67e0e8243e022390f1e2e69562bc1bf3298fa703
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2025-06-01 14:54:32 -0400

    fix: correct description text and improve layout structure in RootLayout component. place theme provider to encapsolate the navbar too

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Poppins, Lato } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-import { ThemeProvider } from 'next-themes';
+import { ThemeProvider } from "next-themes";
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -18,7 +18,7 @@ const lato = Lato({
 
 export const metadata: Metadata = {
   title: "Pet Age Calculator",
-  description: "Determine your pets age in Human years",
+  description: "Determine your pet's age in human years",
 };
 
 export default function RootLayout({
@@ -27,13 +27,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${lato.variable} ${poppins.variable}`}
-      >
-        <Navbar />  
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${lato.variable} ${poppins.variable}`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        {children}
+          <Navbar />
+          {children}
         </ThemeProvider>
       </body>
     </html>
